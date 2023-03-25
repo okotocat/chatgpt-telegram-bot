940feff9b3d2d9ddf6354ddafb455b716a32ca37
okotocat <okotocat@gmail.com>
2023-03-25 21:26:33 +0700

    update

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import telebot
 import openai
 import mysql.connector
+from mysql.connector.conversion import MySQLConverter
 from telebot import util
 from datetime import datetime
 from sys import platform
@@ -9,18 +10,18 @@ from api_keys import openaiapi, telegrambotapi, user, password, host, database
 
 bot = telebot.TeleBot(telegrambotapi)
 openai.api_key = openaiapi
-logfile = "var/log/some_file_you_want"						#change this line
 
 @bot.message_handler(commands=['start','help'])
 def start_message(message):
 	bot.send_message(message.chat.id, 'Просто напиши текстом свой запрос, я отвечу  /// just type your prompt w/o any commands   /// contact:@tttriot')
 @bot.message_handler(content_types=['text'])
 def chat_handler(message):
-	command = message.text
+	command = MySQLConverter().escape(message.text)
 	bot_base_message = bot.send_message(message.chat.id, '♻️[GPT] Loading...')
 	try:
 		gpt_reply = openai.ChatCompletion.create(model='gpt-3.5-turbo', messages=[{'role': 'user', 'content': command}],)
-	except openai.error.OpenAIError as e:					#OpenAI exception bout server load
+		gpt_for_mysql = MySQLConverter().escape(gpt_reply['choices'][0]['message']['content'])
+	except openai.error.OpenAIError as e:				#OpenAI exception bout server load
 		print(e)
 		bot.reply_to(message, text='⚠Heavy load on OpenAI servers! Try again')
 	bot.edit_message_text('✅[GPT] Done!', chat_id=message.chat.id, message_id=bot_base_message.message_id)
@@ -41,29 +42,12 @@ def chat_handler(message):
 				)
 			mycursor = mysqlconnection.cursor()
 			sql = "INSERT INTO main_table (user_id, timestamp, prompt, reply, username) VALUES (%s,%s,%s,%s,%s)"
-			val = (message.from_user.id, str(datetime.fromtimestamp(message.date)), str(command), str(gpt_reply['choices'][0]['message']['content']), str(message.from_user.username))
+			val = (message.from_user.id, str(datetime.fromtimestamp(message.date)), str(command), str(gpt_for_mysql), str(message.from_user.username))
 			mycursor.execute(sql,val)
 			mysqlconnection.commit()
 			mycursor.close()
 			mysqlconnection.close()
 		push_to_db(message)
-		f = open(logfie,"a")
-		f.writelines('\n')
-		f.writelines('userID: ')
-		f.writelines(str(message.from_user.id))
-		f.writelines(' Username: ')
-		f.writelines(str(message.from_user.username))
-		f.writelines(' Date: ')
-		f.writelines(str(datetime.fromtimestamp(message.date)))
-		f.writelines('\n')
-		f.writelines('Prompt: ')
-		f.writelines(str(command))
-		f.writelines('\n')
-		f.writelines('AI reply: ')
-		f.writelines(str(gpt_reply['choices'][0]['message']['content']))
-		f.writelines('\n')
-		f.writelines('---------------------------------------------------------------------------')
-		f.close
 	else:
 		pass
 bot.infinity_polling()
